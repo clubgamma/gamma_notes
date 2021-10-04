13109abb8b984c0bb1442d36b035914a12c313bb
sneha-meto <snehameto@gmail.com>
2021-10-04 22:39:22 +0530

    display pins #1

--- lib/Screens/side_bar.dart
+++ lib/Screens/side_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gamma_keep/Constants/color.dart';
+import 'package:gamma_keep/Screens/pin_notes.dart';
 import '../Constants/color.dart' as color;
 
 class SideBar extends StatefulWidget {
@@ -14,59 +15,59 @@ class _SideBarState extends State<SideBar> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: kBgColor,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0, left: 30.0, bottom: 20.0),
-            child: Row(
-              children: const [
-                Text(
-                  "Gamma Notes",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 23,
-                      color: color.kWhite
+//      backgroundColor: kBgColor,
+      child: Container(
+        color: kBgColor,
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 40.0, left: 30.0, bottom: 20.0),
+              child: Row(
+                children: const [
+                  Text(
+                    "Gamma Notes",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 23,
+                        color: color.kWhite),
                   ),
+                ],
+              ),
+            ),
+            const Divider(
+              height: 1.0,
+              color: Colors.white24,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PinnedNotes()));
+              },
+              child: const ListTile(
+                leading: Icon(
+                  CupertinoIcons.pin,
+                  color: color.kWhite,
                 ),
-              ],
-            ),
-          ),
-
-          const Divider(
-            height: 1.0,
-            color: Colors.white24,
-          ),
-
-          const ListTile(
-            leading: Icon(
-              CupertinoIcons.pin,
-              color: color.kWhite,
-            ),
-            title: Text(
-              "Pinned",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0
+                title: Text(
+                  "Pinned",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
               ),
             ),
-          ),
-
-          const ListTile(
-            leading: Icon(
-              CupertinoIcons.heart,
-              color: color.kWhite,
-            ),
-            title: Text(
-              "Favourites",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0
+            const ListTile(
+              leading: Icon(
+                CupertinoIcons.heart,
+                color: color.kWhite,
+              ),
+              title: Text(
+                "Favourites",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
